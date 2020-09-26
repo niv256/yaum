1fe405c12088e1f380749a48e683cfc9f9d1c63c
niv256 <niv25600@gmail.com>
2020-09-27 01:06:27 +0300

    Add an inline asm outw function

--- src/kernel/tools.c
+++ src/kernel/tools.c
@@ -5,6 +5,10 @@ void outb(uint16_t port, uint8_t val) {
   asm volatile("outb %1, %0" : : "dN"(port), "a"(val));
 }
 
+void outw(uint16_t port, uint16_t val) {
+  asm volatile("outw %1, %0" : : "dN"(port), "a"(val));
+}
+
 uint8_t inb(uint16_t port) {
   uint8_t ret;
   asm volatile("inb %1, %0" : "=a"(ret) : "Nd"(port));
