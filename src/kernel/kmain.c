9d4e92369100605fbccab3ba8eb6bbaf7cfa64f6
niv256 <niv25600@gmail.com>
2020-10-02 02:19:55 +0300

    Remove useless kmain.c comments and other simple changes

--- src/kernel/kmain.c
+++ src/kernel/kmain.c
@@ -3,25 +3,16 @@
 #include "paging.h"
 #include "screen.h"
 #include "timer.h"
-#include <stddef.h>
 
-/* Check if the compiler thinks you are targeting the wrong operating system. */
-#if defined(__linux__)
-#error \
-    "You are not using a cross-compiler, you will most certainly run into trouble"
+#if defined(__linux__) || !defined(__i686__)
+#error "Either not using a cross-compiler or not an x86 elf compiler"
 #endif
 
-/* This tutorial will only work for the 32-bit ix86 targets. */
-#if !defined(__i386__)
-#error "This tutorial needs to be compiled with a ix86-elf compiler"
-#endif
 
 int kmain(void) {
-  /* initialize terminal interface */
   terminal_initialize();
   terminal_writestring("yaum: big siber os.\n");
 
-  /* initialize the gdt */
   gdt_init();
   terminal_writestring("gdt initialized.\n");
 
@@ -31,20 +22,19 @@ int kmain(void) {
   init_paging();
   terminal_writestring("paging initialized.\n");
 
-  /* initialize the idt */
   idt_init();
   terminal_writestring("idt initialized.\n");
 
   timer_init();
   terminal_writestring("timer initialized.\n");
 
-  /* initialize the keyboard */
   keyboard_init();
   terminal_writestring("keyboard initialized.\n");
 
-  terminal_writestring("> ");
+  terminal_writestring("\nshell:\n> ");
 
   for (;;)
     ;
+
   return 0;
 }
