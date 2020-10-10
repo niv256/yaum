98f01c6763e72b28cec04c5d09ec7e66574a79f8
niv256 <niv25600@gmail.com>
2020-10-10 20:00:27 +0300

    Add arguments for syscalls
    The current implemintation goes as follows:
    syscall number:       eax
    arguments (in order): ebx
                          ecx
                          edx
                          esi
                          edi
    
    Also changed very_important_syscall to print the caller ebx's value.

--- kernel/arch/i386/userspace_func.asm
+++ kernel/arch/i386/userspace_func.asm
@@ -4,6 +4,7 @@ global userspace_func
 
 userspace_func:
   mov eax, 0 ; very important syscall
+  mov ebx, 0xcafebabe
   int 0x80
   jmp $
   ret ; never gets here
--- kernel/syscall/syscall.c
+++ kernel/syscall/syscall.c
@@ -2,10 +2,18 @@
 #include <io/screen.h>
 #include <syscall/syscall.h>
 
-static int very_important_syscall(void);
-static void syscall(registers_t *regs);
+typedef struct syscall_args {
+  uint32_t ebx;
+  uint32_t ecx;
+  uint32_t edx;
+  uint32_t esi;
+  uint32_t edi;
+} syscall_args_t;
 
-typedef int (*syscall_t)(void);
+typedef int (*syscall_t)(syscall_args_t);
+
+static int very_important_syscall(syscall_args_t args);
+static void syscall(registers_t *regs);
 
 syscall_t syscall_table[SYSCALLS] = {very_important_syscall};
 
@@ -13,15 +21,20 @@ void init_syscall(void) {
   setup_isr_callback(SYSCALL_INT, (isr_t)syscall);
 }
 
-static int very_important_syscall(void) {
+static int very_important_syscall(syscall_args_t args) {
   terminal_writestring("very important syscall called!\n");
+  terminal_writestring("ebx:\n");
+  terminal_writehex(args.ebx);
+  terminal_newline();
   return 0xdeadbeef;
 }
 
 static void syscall(registers_t *regs) {
   uint32_t syscall_num = regs->eax;
+  syscall_args_t args = {regs->ebx, regs->ecx, regs->edx, regs->esi, regs->edi};
+
   if (syscall_num < SYSCALLS && syscall_table[syscall_num]) {
-    regs->eax = syscall_table[syscall_num]();
+    regs->eax = syscall_table[syscall_num](args);
   } else {
     terminal_writestring("unknown syscall:\n");
     terminal_writehex(syscall_num);
