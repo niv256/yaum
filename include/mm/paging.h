76a4d6464be8c9d7727df2e61e41e6ceb5350576
niv256 <niv25600@gmail.com>
2020-10-10 20:37:53 +0300

    Add a PAGE_SIZE macro

--- include/mm/paging.h
+++ include/mm/paging.h
@@ -3,15 +3,17 @@
 
 #include <stdint.h>
 
+#define PAGE_SIZE 0x1000
+
 typedef uint32_t pte_t;
 typedef uint32_t pde_t;
 
 #define PTE_IN_TABLE     1024
 #define PDE_IN_DIRECTORY 1024
 
-typedef pte_t page_table_t[PTE_IN_TABLE] __attribute__((aligned(0x1000)));
+typedef pte_t page_table_t[PTE_IN_TABLE] __attribute__((aligned(PAGE_SIZE)));
 typedef pde_t page_directory_t[PDE_IN_DIRECTORY]
-    __attribute__((aligned(0x1000)));
+    __attribute__((aligned(PAGE_SIZE)));
 
 #define PAGE_PRESENT 0x1
 #define PAGE_WRITE   0x2
