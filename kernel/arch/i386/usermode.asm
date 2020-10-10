03f2ef4086ef3ed4b8ed2c699e4b161e222e6154
niv256 <niv25600@gmail.com>
2020-10-10 20:54:26 +0300

    Cleanup ring 3 jump code

--- kernel/arch/i386/usermode.asm
+++ kernel/arch/i386/usermode.asm
@@ -6,6 +6,7 @@ SELECTOR_SHIFT_AMOUNT equ 3
 USER_CODE_INDEX       equ 3
 USER_DATA_INDEX       equ 4
 DPL3                  equ 3
+IF_FLAG               equ 0x200
 
 USER_CODE equ ((USER_CODE_INDEX << SELECTOR_SHIFT_AMOUNT) |  DPL3)
 USER_DATA equ ((USER_DATA_INDEX << SELECTOR_SHIFT_AMOUNT) |  DPL3)
@@ -14,16 +15,23 @@ global jump_usermode
 
 jump_usermode:
   mov ecx, [esp+4]
-  mov ax, 0x23
+
+  mov ax, USER_DATA
   mov ds, ax
   mov es, ax
   mov fs, ax
   mov gs, ax
-
-  mov eax, esp
-  push 0x23
   push eax
-  push 0x202 ; eflags: IF enabled
-  push 0x1b
+
+  push esp
+
+  pushf
+  pop eax
+  or eax, IF_FLAG
+  push eax
+
+  push USER_CODE
+
   push ecx
+
   iret
