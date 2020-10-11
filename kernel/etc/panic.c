091cd005fd868d13ee2df1aa7cb5c45d2eb962bd
nimrodn <nimrodnaim@gmail.com>
2020-10-11 14:11:53 -0700

    ebp w/ret backtrace

--- kernel/etc/panic.c
+++ kernel/etc/panic.c
@@ -15,4 +15,18 @@ void panic(char *panic_msg, char *file, int line) {
 }
 
 static void print_info(void) {
+    /* trace ebp and return adress of functions */
+    uint32_t *ebp;
+    uint32_t *ret;
+
+    asm("movl %%ebp, %0;": "=r" (ebp)); //magic
+    ebp = (int *) *ebp; //de-refrence once so we won't print the value of the backtrace function
+
+    while(ebp != NULL) {
+        ret = (int *) *(ebp + 1); //pointers fun
+        printf("value of ebp is %p\t value of ret is %p\n", ebp, ret);
+
+        ebp = (int *) *ebp;
+    }
+
 }
