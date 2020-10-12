80b18df9abaec6529dbbe2527870d805055dde6c
OfekMarks <48588009+OfekMarks@users.noreply.github.com>
2020-10-12 17:40:47 +0300

    ftoa #2

--- libc/stdlib/ftoa.c
+++ libc/stdlib/ftoa.c
@@ -1,41 +1,34 @@
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
 #define BASE 10
 
 char *ftoa(double number, int ndigit, char *buf)
 {
-    if(ndigit < 0) // no support for negative digits after the decimal point
-        ndigit = 0;
+  if(ndigit < 0) // no support for negative digits after the decimal point
+    ndigit = 0;
 
-    // percision
-    double percision = 0.5;
-    for(int i = ndigit; i; i--)
-        percision /= BASE;
-    number += (number > 0) ? percision : -percision;
+  // percision
+  double percision = 0.5;
+  for(int i = ndigit; i; i--)
+    percision /= 10;
+  number += (number > 0) ? percision : -percision;
 
-    int int_part = (int)(number);
-    itoa(int_part, buf, BASE); // getting integer part using itoa
+  int int_part = (int)(number);
+  itoa(int_part, buf, BASE); // getting integer part using itoa
     
-    // the decimal part of a negative number is the same as its opposite
-    if(number < 0)
-    {
-        number = -number;
-        int_part = -int_part;
-    }
-    
-    // after the end of the integer part will be the decimal part
-    char *fraction = buf + strlen(buf);
-    *fraction++ = '.'; // decimal point divides both parts
+  // getting the decimal part's digits as an integer, in order to use atoi again
+  number -= int_part;
+  for(; ndigit; ndigit--) // we nedd only $ndigits digits of the decimal part
+    number *= BASE;
+  int_part = abs((int) number); // the decimal part is non-negative
 
-    number = (number - int_part) * BASE; // get the first decimal digit
-    for(; ndigit > 0 && number; ndigit--, number = (number - (int)(number)) * BASE)
-        *fraction++ = '0' + (int)(number);
-
-    // if there's no decimal part, we'll delete the decimal point
-    if(*(fraction - 1) == '.')
-        fraction--;
-
-    *fraction = '\0'; // end of string
-
+  if(!int_part) // if the decimal part is 0, we are done with converting the number
     return buf;
+
+  char *fraction = buf + strlen(buf);
+  *fraction++ = '.'; // decimal point divides both parts
+
+  itoa(int_part, fraction, BASE); // converting the decimal digits into a string
+  return buf;
 }
