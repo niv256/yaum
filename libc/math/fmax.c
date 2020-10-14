feb4cd0d349cd82ceada229e3e327f671e36fd21
OfekMarks <48588009+OfekMarks@users.noreply.github.com>
2020-10-14 14:06:45 +0300

    getting min / max between 2 double values

---
+++ libc/math/fmax.c
@@ -0,0 +1,8 @@
+#include <math.h>
+
+int fmax(double x, double y)
+{
+  if(x > y)
+    return x;
+  return y;
+}
---
+++ libc/math/fmin.c
@@ -0,0 +1,8 @@
+#include <math.h>
+
+double fmin(double x, double y)
+{
+  if(x < y)
+    return x;
+  return y;
+}
