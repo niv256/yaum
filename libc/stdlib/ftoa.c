aab1f4dc79aff4cb93006d21c92b591beb82966e
niv256 <niv25600@gmail.com>
2020-10-14 19:07:05 +0300

    Clean ftoa

--- libc/stdlib/ftoa.c
+++ libc/stdlib/ftoa.c
@@ -3,31 +3,37 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BASE          10
-#define MAX_PRECISION 10
+int get_decimal_places(float number);
 
-char *ftoa(double number, int ndigit, char *buf) {
-  // no support for negative digits after the decimal point
-  ndigit = fmax(0, ndigit);
+char *ftoa(float number, int ndigit, char *buf) {
+  ndigit = fmin((uint32_t)ndigit, get_decimal_places(number));
 
-  uint32_t int_part = (int)(number);
-  itoa(int_part, buf, BASE); // getting integer part using itoa
+  if (number < 0) {
+    *buf++ = '-';
+    number = fabs(number);
+  }
 
-  // getting the decimal part's digits as an integer, in order to use atoi again
+  uint32_t int_part = (uint32_t)(number);
+  itoa(int_part, buf, 10);
+
   number -= int_part;
-  number = fabs(number); // and we continue with the absolute value
 
-  // we need only $ndigits digits of the decimal part
-  int_part =
-      (uint32_t)(number * __builtin_powf(BASE, fmin(ndigit, MAX_PRECISION)));
-  if (ndigit) { // if the decimal part is 0, we are done with converting the
-                // number
-    char *fraction = buf + strlen(buf);
-    *fraction++    = '.'; // decimal point divides both parts
-
-    utoa(int_part, fraction,
-         BASE); // converting the decimal digits into a string
+  int_part = (uint32_t)(number * pow(10, ndigit));
+  if (ndigit) {
+    buf += strlen(buf);
+    *buf++ = '.';
+    utoa(int_part, buf, 10);
   }
 
   return buf;
+}
+
+int get_decimal_places(float number) {
+  int count = 0;
+
+  while ((number = number * 10 - (int)number * 10)) {
+    count++;
+  }
+
+  return count;
 }
